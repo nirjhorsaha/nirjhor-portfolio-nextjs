831a906cf1d74f8b42150d866104ebff14878093
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-10-24 18:51:09 +0600

    [Added] timeline in experience section

--- src/Modules/Home/Sections/Worked/Worked.tsx
+++ src/Modules/Home/Sections/Worked/Worked.tsx
@@ -1,129 +1,69 @@
 /* eslint-disable prettier/prettier */
-import React from 'react'
-import { motion } from 'framer-motion'
-import Image from 'next/image'
-import { Link } from '@nextui-org/link'
-import { IoMdArrowDropright } from 'react-icons/io'
+import { Link } from '@nextui-org/link';
+import { IoMdArrowDropright } from 'react-icons/io';
 
-import Content from './partials/Content'
+import { experienceData } from './config/constants';
 
-import { fadeLeft, motionStep } from '@/src/config/motion'
-import { Timeline } from '@/src/components/UI/Aceternity/timeline'
+import { Timeline } from '@/src/components/UI/Aceternity/timeline';
+import { GradualSpacing } from '@/src/components/UI/MagicUI/GradualSpacing';
+import { Experience } from '@/src/config/types';
+
+// ExperienceItem Component 
+const ExperienceItem: React.FC<Experience> = ({ title, company, period, responsibilities }) => (
+  <div>
+    <h1 className="text-xl font-medium text-slate-300">
+      {title} {''}
+      <Link
+        className="text-sky-300 border-b border-b-sky-300 border-dotted"
+        href={company.url}
+        target="_blank"
+      >
+        {""} @{company.name}
+      </Link>
+    </h1>
+    <p className="text-slate-400 font-medium mt-1">{period}</p>
+    <ul className="space-y-5 mt-5 max-w-xl">
+      {responsibilities.map((item, index) => (
+        <li key={index} className="gap-1 flex text-slate-400 select-none">
+          <span className="min-w-10">
+            <IoMdArrowDropright className="text-sky-400 text-[1.3rem]" />
+          </span>
+          <p>{item}</p>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 const Worked = () => {
-
-  const data = [
-    {
-      title: "2023",
-      content: (
-        <div >
-          <div>
-            <h1 className="text-xl font-medium text-slate-300">
-              Jr. Software Engineer {''}
-              <Link
-                className="text-sky-300 border-b border-b-sky-300 border-dotted "
-                href='https://hatechz.com/'
-                target="_blank"
-              >
-                {""} @{"HATechZ"}
-              </Link>
-            </h1>
-            <p className="text-slate-400 font-medium mt-1"> {"Jun'23 - Present"} </p>
-          </div>
-          <ul className="space-y-5 mt-5 max-w-xl">
-            <li
-              className="gap-1 flex text-slate-400 select-none"
-            >
-              <span className="min-w-10">
-                <IoMdArrowDropright className="text-sky-400 text-[1.3rem]" />
-              </span>
-              <p>Developed and maintained high-performance, responsive web applications using JavaScript, React.js, and Next.js, while also building and optimizing cross-platform mobile applications with Flutter, all within an agile environment, delivering features and fixes under tight deadlines and adhering to industry best practices.</p>
-            </li>
-          </ul>
-        </div>
-      ),
-    },
-    {
-      title: "2022",
-      content: (
-        <div >
-          <div>
-            <h1 className="text-xl font-medium text-slate-300">
-            Associate Member
-              <Link
-                className="text-sky-300 border-b border-b-sky-300 border-dotted md:ml-2"
-                href='http://clubs.daffodilvarsity.edu.bd/club/diucpc'
-                target="_blank"
-              >
-                {" "}
-                @{"DIU Computer Programming Club"}
-              </Link>
-            </h1>
-            <p className="text-slate-400 font-medium mt-1"> {"Feb'20 - Dec'22"} </p>
-          </div>
-          <ul className="space-y-5 mt-5 max-w-xl">
-            <li
-              className="gap-1 flex text-slate-400 select-none"
-            >
-              <span className="min-w-10">
-                <IoMdArrowDropright className="text-sky-400 text-[1.3rem]" />
-              </span>
-              <p>Contributed to the promotion of club activities, significantly increasing engagement and membership.</p>
-            </li>
-          </ul>
-        </div>
-      ),
-    },
-    {
-      title: "2020",
-      content: (
-        <div >
-          <div>
-            <h1 className="text-xl font-medium text-slate-300">
-              Campus Ambassador
-              <Link
-                className="text-sky-300 border-b border-b-sky-300 border-dotted md:ml-2"
-                href='https://www.admissionassistant.com.bd/'
-                target="_blank"
-              >
-                {" "}
-                @{"Admission Assistant"}
-              </Link>
-            </h1>
-            <p className="text-slate-400 font-medium mt-1"> {"Mar'20 - Dec'20"} </p>
-          </div>
-          <ul className="space-y-5 mt-5 max-w-xl">
-            <li
-              className="gap-1 flex text-slate-400 select-none"
-            >
-              <span className="min-w-10">
-                <IoMdArrowDropright className="text-sky-400 text-[1.3rem]" />
-              </span>
-              <p>Worked alongside fellow ambassadors to organize promotional events</p>
-            </li>
-          </ul>
-        </div>
-      ),
-    },
-   
-  ];
-
   return (
-    <section className='container' id='experience' >
-      <motion.h1 variants={fadeLeft} {...motionStep} className='flex items-center gap-2 text-lg md:text-3xl font-medium text-slate-300' >
-        <span className='text-sky-400 font-mono' >02. </span>
-        Changelog from my journey
-      </motion.h1>
-
-      {/* <div className='mt-14' >
-        <Content />
-      </div> */}
-
+    <section className='container' id='experience'>
+      <GradualSpacing
+        className="text-lg md:text-5xl font-medium font-display -tracking-widest text-black dark:text-sky-400 md:leading-[5rem]"
+        text="Changelog from my journey"
+      />
       <div className="w-full">
-        <Timeline data={data} />
+        <Timeline
+          data={experienceData.map((yearEntry) => ({
+            title: yearEntry.year,
+            content: (
+              <div>
+                {yearEntry.experiences.map((exp, index) => (
+                  <ExperienceItem
+                    key={index}
+                    company={exp.company}
+                    period={exp.period}
+                    responsibilities={exp.responsibilities}
+                    title={exp.title}
+                  />
+                ))}
+              </div>
+            ),
+          }))}
+        />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Worked
+export default Worked;
--- src/Modules/Home/Sections/Worked/config/constants.ts
+++ src/Modules/Home/Sections/Worked/config/constants.ts
@@ -1,41 +1,44 @@
 /* eslint-disable prettier/prettier */
-import { WorkedTypes } from "./types";
 
-// worked data
-export const worked_data: WorkedTypes[] = [
+export const experienceData = [
   {
-    id: 0,
-    label: "HATechZ",
-    description: [
-      "Developed and maintained high-performance, responsive web applications using JavaScript, React.js, and Next.js, while also building and optimizing cross-platform mobile applications with Flutter, all within an agile environment, delivering features and fixes under tight deadlines and adhering to industry best practices.",
+    year: "2023",
+    experiences: [
+      {
+        title: "Jr. Software Engineer",
+        company: { name: "HATechZ", url: 'https://hatechz.com/' },
+        period: "Jun'23 - Present",
+        responsibilities: [
+          "Developed and maintained high-performance, responsive web applications using JavaScript, React.js, and Next.js.",
+          "Building and optimizing cross-platform mobile applications with Flutter, all within an agile environment."
+        ],
+      },
     ],
-    deadline: "Jun'23 - Present",
-    title: "Jr. Software Engineer",
-    company: "HATechZ",
-    url: "https://hatechz.com/",
   },
   {
-    id: 1,
-    label: "DIU CPC",
-    description: [
-      "Engaged with the student community to promote programming and coding events.",
-      "Assisted in organizing coding competitions and workshops."
+    year: "2022",
+    experiences: [
+      {
+        title: "Associate Member",
+        company: { name: "DIU Computer Programming Club", url: 'http://clubs.daffodilvarsity.edu.bd/club/diucpc' },
+        period: "Feb'20 - Dec'22",
+        responsibilities: [
+          "Contributed to the promotion of club activities, significantly increasing engagement and membership."
+        ],
+      },
     ],
-    deadline: "Feb'20 - Dec'22",
-    title: "Associate Member",
-    company: "DIU Computer Programming Club",
-    url: "http://clubs.daffodilvarsity.edu.bd/club/diucpc",
   },
   {
-    id: 2,
-    label: "Admission Assistant",
-    description: [
-      "Served as a liaison between the university and prospective students, providing guidance on the admissions process.",
-      "Organized and participated in university promotional events to attract new applicants."
+    year: "2020",
+    experiences: [
+      {
+        title: "Campus Ambassador",
+        company: { name: "Admission Assistant", url: 'https://www.admissionassistant.com.bd/' },
+        period: "Mar'20 - Dec'20",
+        responsibilities: [
+          "Worked alongside fellow ambassadors to organize promotional events."
+        ],
+      },
     ],
-    deadline: "Mar'20 - Dec'20",
-    title: "Campus Ambassador",
-    company: "Admission Assistant",
-    url: "https://www.admissionassistant.com.bd/",
   },
 ];
