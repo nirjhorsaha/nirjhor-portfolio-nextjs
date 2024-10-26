5e04801433151be499589f7689a255dda42ced34
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-10-26 20:36:35 +0600

    [Implement] custom button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,17 @@
 /* eslint-disable prettier/prettier */
 
-interface ButtonProps {
-  label: string;
-  onClick?: () => void;
-  className?: string;
-  type?: 'button' | 'submit' | 'reset';
-  icon?: React.ReactNode; 
-}
+import { ButtonProps } from "../config/types";
 
 const Button: React.FC<ButtonProps> = ({
   label,
   onClick,
   className = '',
   type = 'button',
-  icon, 
+  icon,
 }) => {
   return (
     <button
-      className={`bg-slate-800 text-white no-underline group cursor-pointer relative shadow-2xl dark:shadow-zinc-900 rounded-xl p-px text-base leading-7 inline-block tracking-wide ${className}`}
+      className={`bg-slate-100 dark:bg-slate-800 dark:text-white no-underline group cursor-pointer relative shadow-2xl dark:shadow-zinc-900 rounded-xl p-px text-base leading-7 inline-block tracking-wide ${className}`}
       type={type}
       onClick={onClick}
     >
@@ -25,8 +19,8 @@ const Button: React.FC<ButtonProps> = ({
         <span className="absolute inset-0 rounded-full opacity-0 transition-opacity duration-500 group-hover:opacity-50" />
       </span>
       <div className="relative flex space-x-2 items-center z-10 rounded-xl py-2 px-6 ring-1 ring-white/10">
-        {icon && <span className="flex items-center">{icon}</span>} 
-        <span>{label}</span>
+        <span className="tracking-wide">{label}</span>
+        {icon && <span className="flex items-center">{icon}</span>}
       </div>
       <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-sky-400/0 via-sky-400/90 to-sky-400/0 transition-opacity duration-500 group-hover:opacity-40" />
     </button>
