a33e9cc90725d9ecac656ce774e3a142d9d42a06
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-10-26 20:33:20 +0600

    [Updated] provider

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,11 +6,3 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-
-// export default function twFocusClass(hasRing = false) {
-//   if (!hasRing) {
-//     return "focus:outline-none";
-//   }
-
-//   return "focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary dark:focus:ring-offset-0";
-// }
